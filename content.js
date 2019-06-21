ff3cbcbf9a712f26af81e6ec36156235b758e565
EricHeredia <EricDavidHeredia@gmail.com>
2019-06-20 21:10:10 -0500

    removed useless logs

--- content.js
+++ content.js
@@ -11,17 +11,12 @@ chrome.runtime.onMessage.addListener(
 const findKeyRE = /(([A-Z]|[0-9]){5}-){2}([A-Z]|[0-9]){5}/g
 
 if (document.body.innerHTML.match(findKeyRE) != null) {
-  console.clear()
   const match = document.body.innerHTML.match(findKeyRE)
-  const lastIndex = match[match.length-1]
 
   chrome.runtime.sendMessage({"message": "key_found", "amount": match.length})
 
   match.forEach((key) => {
-    console.log(key)
   })
 } else {
   chrome.runtime.sendMessage({"message": "key_found", "amount": ""})
-  console.clear()
-  console.log('No Steam keys on page.')
 }
